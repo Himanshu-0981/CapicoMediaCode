c9c30eecb6db5bd90f88374382e8d2ac2ffa5512
Himanshu <kumarhimanshu2219@gmail.com>
2022-06-29 23:37:10 +0530

    adding comments

--- Script/toggle.js
+++ Script/toggle.js
@@ -1,6 +1,8 @@
 let toggleBtn = document.querySelector('.nav-toggle-btn');
 let nav = document.querySelector('.navbar');
 
+// toogle function
+
 let toggleFunc = () => {
     nav.classList.toggle('active');
 }
